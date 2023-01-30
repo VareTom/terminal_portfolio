1fde58ef818de1740003a5145d9bc56243590562
tomvarewyck <tvarewyck@gmail.com>
2023-01-30 09:16:37 +0100

    Add `cd` command in help

--- src/lib/Help.svelte
+++ src/lib/Help.svelte
@@ -9,7 +9,7 @@
     {name: 'reset', text: 'Reset the terminal with the initial state.'},
     {name: 'clear', text: 'Remove all previous lines in the terminal.'},
     {name: 'ls', text: 'Display a list of files and sub-directories in the current directory.'},
-    // {name: 'cd', text: 'Change current working directory.'},
+    {name: 'cd', text: 'Change current working directory.'},
     {name: 'cat', text: 'Display the content of an entry.'},
     {name: 'history', text: 'Display all inserted commands.'}
   ]
